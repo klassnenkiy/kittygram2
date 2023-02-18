54391c5786f88450448cb7ca7b1601a08cf58a1f
Stanislav Tyulyagin <klassnenkiy@gmail.com>
2023-02-19 00:52:45 +0300

    1

--- cats/views.py
+++ cats/views.py
@@ -9,7 +9,7 @@ class CatViewSet(viewsets.ModelViewSet):
     serializer_class = CatSerializer
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
+        serializer.save(owner=self.request.user)
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
